6d747d704c7b160e3bd2510f48a00b09340f1e54
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-04-28 20:46:06 +0900

    posts.js second modify

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const Post = require("../schemas/post.js");
+const { default: mongoose } = require("mongoose");
 const router = express.Router();
 
 
 // 전체 게시글 목록 조회 API
 router.get("/posts", async (req, res) => {
     // 제목, 작성자명, 작성 날짜 조회
-    const posts = await Post.find({}).select("postId user title createdAt")
+    const posts = await Post.find({}, { _id: 0, postId: 1, user: 1, title: 1, createdAt: 1 })
     // 날짜 내림차순으로 정렬
     const result = posts.sort((a, b) => a.createdAt - b.createdAt).reverse();
 
@@ -15,12 +16,13 @@ router.get("/posts", async (req, res) => {
 
 // 게시글 작성 API
 router.post("/posts", async (req, res) => {
+    const postId = new mongoose.Types.ObjectId();
     const { user, password, title, content } = req.body;
     // 데이터가 하나라도 없는 경우
     if (!(user && password && title && content)) {
         return res.status(400).json({ message: "데이터 형식이 올바르지 않습니다." })
     }
-    await Post.create({ user, password, title, content })
+    await Post.create({ postId, user, password, title, content, createdAt: new Date() })
 
     res.json({ message: "게시글을 생성하였습니다." })
 });
@@ -30,7 +32,7 @@ router.get("/posts/:_postId", async (req, res) => {
     // 제목, 작성자명, 작성 날짜, 작성 내용을 조회하기
     // params로 받아오기
     const { _postId } = req.params;
-    const post = await Post.find({ _postId });
+    const post = await Post.findOne({ postId: _postId }, { _id: 0, postsId: 1, user: 1, title: 1, content: 1, createdAt: 1 });
     // post 존재 여부
     if (post.length) {
         res.json({ post: post })
@@ -43,8 +45,8 @@ router.get("/posts/:_postId", async (req, res) => {
 router.put("/posts/:_postId", async (req, res) => {
     // params로 받아오기
     const { _postId } = req.params;
-    // 
-    const amho = Post.find({ _postId }).select("password")
+    // amho로 기존에 password 받아오기
+    const amho = Post.find({ postId: _postId }, {_id:0, password:1})
     // 입력 받은 값들 body로
     const { password, title, content } = req.body;
 
@@ -66,7 +68,7 @@ router.put("/posts/:_postId", async (req, res) => {
 // 게시글 삭제 API
 router.delete("/posts/:_postId", async (req, res) => {
     const { _postId } = req.params;
-    const amho = Post.find({ _postId }).select("password")
+    const amho = Post.findOne({ postId: _postId }, {_id: 0, password: 1})
     const { password } = req.body;
 
     // 입력이 없을 경우

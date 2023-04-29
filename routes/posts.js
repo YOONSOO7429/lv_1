876c7ff7262a19dca8ae89cc466859dc0731dadd
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-04-29 13:22:26 +0900

    posts.js final modify

--- routes/posts.js
+++ routes/posts.js
@@ -34,12 +34,12 @@ router.get("/posts/:_postId", async (req, res) => {
     // 제목, 작성자명, 작성 날짜, 작성 내용을 조회하기
     // params로 받아오기
     const { _postId } = req.params;
-    const post = await Post.findOne({ postId: _postId }, { _id: 0, postsId: 1, user: 1, title: 1, content: 1, createdAt: 1 });
+    const post = await Post.findOne({ postId: _postId }, { _id: 0, __v: 0, password: 0 });
     // post 존재 여부
-    if (post.length) {
-        res.json({ post: post })
-    } else {
+    if (!post) {
         return res.status(400).json({ message: "데이터 형식이 올바르지 않습니다." })
+    } else {
+        res.json({ "data": post })
     }
 });
 
@@ -48,19 +48,20 @@ router.put("/posts/:_postId", async (req, res) => {
     // params로 받아오기
     const { _postId } = req.params;
     // amho로 기존에 password 받아오기
-    const amho = Post.find({ postId: _postId }, {_id:0, password:1})
+    const amho = await Post.findOne({ postId: _postId }, { _id: 0, __v:0 })
     // 입력 받은 값들 body로
     const { password, title, content } = req.body;
+    
+
 
     // 입력받은 값이 하나라도 없을 경우
     if (!(password && title && content)) {
-        res.status(400).json({ message: "데이터 형식이 올바르지 않습니다." })
-        return
+        return res.status(400).json({ message: "데이터 형식이 올바르지 않습니다." })
     }
 
     // 암호에 따라 수정 여부
-    if (amho === password) {
-        await Post.updateOne({ $set: { content } })     // 일치할 경우 content 수정
+    if (amho.password == password) {
+        await Post.updateOne({ postId: _postId }, { $set: { content } })     // 일치할 경우 content 수정
         return res.json({ message: "게시글을 수정하였습니다." })
     } else {
         return res.status(404).json({ message: "게시글 조회에 실패하였습니다." })
@@ -73,7 +74,7 @@ router.delete("/posts/:_postId", async (req, res) => {
     // params로 받아오기
     const { _postId } = req.params;
     // amho로 기존에 password 받아오기
-    const amho = Post.findOne({ postId: _postId }, {_id: 0, password: 1})
+    const amho = await Post.findOne({ postId: _postId }, { _id: 0, __v:0 })
     // 입력 받은 값들 body로
     const { password } = req.body;
 
@@ -83,8 +84,8 @@ router.delete("/posts/:_postId", async (req, res) => {
     }
 
     // 암호에 따라 게시글 삭제 여부
-    if (amho === password) {
-        await Post.deleteOne({ _postId });  // 암호가 맞다면 _postId에 해당하는 글 삭제
+    if (amho.password == password) {
+        await Post.deleteOne({ postId: _postId });  // 암호가 맞다면 _postId에 해당하는 글 삭제
         return res.json({ message: "게시글을 삭제하였습니다." })
     } else {
         return res.status(404).json({ message: "게시글 조회에 실패하였습니다." })

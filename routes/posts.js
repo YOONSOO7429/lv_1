6ae65fce26824505194dd855e55122d306c7006f
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-05-06 19:39:10 +0900

    posts.js modify

--- routes/posts.js
+++ routes/posts.js
@@ -1,33 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const Post = require("../schemas/post.js");
-const User = require("../schemas/user.js")
+const { Posts } = require("../models");
+const { Op } = require('sequelize');
 const authMiddleware = require("../middlewares/auth-middleware.js");
-const mongoose = require("mongoose");
 
-
-// 1. 전체 게시글 목록 조회 API
-//      @ 제목, 작성자명(nickname), 작성 날짜를 조회하기
-//      @ 작성 날짜 기준으로 내림차순 정렬하기
-router.get("/posts", async (req, res) => {
-    try {
-        // 게시글 전체 조회
-        const posts = await Post.find({}, { _id: false, content: false, __v: false, }).sort({ createdAt: -1 });
-        res.json({ posts });
-    }
-    // 예외 케이스에서 처리하지 못한 에러
-    catch {
-        return res.status(400).json({ errorMessage: "게시글 조회에 실패하였습니다." })
-    }
-})
-
-// 2. 게시글 작성 API
+// 1. 게시글 작성 API
 //      @ 토큰을 검사하여, 유효한 토큰일 경우에만 게시글 작성 가능
 //      @ 제목, 작성 내용을 입력하기
 router.post("/posts", authMiddleware, async (req, res) => {
-    const { userId, nickname } = res.locals.user
-    // ObjectId로 postId로 Id부여하기
-    const postId = new mongoose.Types.ObjectId();
+    const { userId } = res.locals.user
     // body로 작성내용 받기
     const { title, content } = req.body;
 
@@ -42,7 +23,7 @@ router.post("/posts", authMiddleware, async (req, res) => {
 
     try {
         // 게시글 작성
-        await Post.create({ UserId: userId, postId, nickname: nickname, title, content, createdAt: new Date(), updatedAt: new Date() });
+        await Posts.create({ userId: userId, title, content });
         return res.status(201).json({ message: "게시글 작성에 성공하였습니다." });
     }
     // 예외 케이스에서 처리하지 못한 에러
@@ -51,14 +32,35 @@ router.post("/posts", authMiddleware, async (req, res) => {
     }
 });
 
+// 2. 전체 게시글 목록 조회 API
+//      @ 제목, 작성자명(nickname), 작성 날짜를 조회하기
+//      @ 작성 날짜 기준으로 내림차순 정렬하기
+router.get("/posts", async (req, res) => {
+    try {
+        // 게시글 전체 조회
+        const posts = await Posts.findAll({
+            attributes: ['postId', 'userId', 'title', 'createdAt', 'updatedAt'],
+            order: [['createdAt', 'DESC']]
+        });
+        res.json({ posts });
+    }
+    // 예외 케이스에서 처리하지 못한 에러
+    catch {
+        return res.status(400).json({ errorMessage: "게시글 조회에 실패하였습니다." })
+    }
+})
+
 // 3. 게시글 조회 API
 //      @제목, 작성자명(nickname), 작성 날짜, 작성 내용을 조회하기
 router.get("/posts/:postId", async (req, res) => {
     // params로 받아오기
     const { postId } = req.params;
     try {
-        const post = await Post.findOne({ postId: postId }, { _id: 0, __v: 0, password: 0 });
-        return res.status(200).json({ post: post })
+        const post = await Posts.findOne({
+            attributes: ['postId', 'userId', 'title', 'content', 'createdAt', 'updatedAt'],
+            where: { postId }
+        });
+        return res.status(200).json({ post })
     }
     // 예외 케이스에서 처리하지 못한 에러
     catch {
@@ -73,7 +75,7 @@ router.put("/posts/:postId", authMiddleware, async (req, res) => {
     // params로 받아오기
     const { postId } = req.params;
     // post 조회하기
-    const post = await Post.findOne({ postId: postId }, { _id: 0, __v: 0 })
+    const post = await Posts.findOne({ where: { postId } })
     // 입력 받은 값들 body로
     const { title, content } = req.body;
 
@@ -90,8 +92,14 @@ router.put("/posts/:postId", authMiddleware, async (req, res) => {
     }
 
     try {
-        await Post.updateOne({ postId: postId }, { $set: { title, content, updatedAt: new Date() } })
-        res.status(200).json({ message: "게시글을 수정하였습니다." })
+        const updateCount = await Posts.update(
+            { title, content },
+            { where: { postId, userId: userId } });
+
+        if (updateCount < 1) {
+            return res.status(401).json({ errorMessage: "게시글이 정상적으로 수정되지 않았습니다." })
+        }
+        return res.status(200).json({ message: "게시글을 수정하였습니다." })
     }
     // 예외 케이스에서 처리하지 못한 에러
     catch (error) {
@@ -105,7 +113,7 @@ router.delete("/posts/:postId", authMiddleware, async (req, res) => {
     // params로 받아오기
     const { postId } = req.params;
     // post 조회하기
-    const post = await Post.findOne({ postId: postId }, { _id: 0, __v: 0 })
+    const post = await Posts.findByPk(postId)
 
     // error
     if (!post) {
@@ -117,8 +125,11 @@ router.delete("/posts/:postId", authMiddleware, async (req, res) => {
 
     try {
         // 게시글 삭제
-        await Post.deleteOne({ postId: postId })
-        res.status(200).json({ message: "게시글을 삭제하였습니다." })
+        const deleteCount = await Posts.destroy({ where: { postId } })
+        if(deleteCount<1) {
+            return res.status(401).json({ errorMessage: "게시글이 정상적으로 삭제되지 않았습니다."})
+        }
+        return res.status(200).json({ message: "게시글을 삭제하였습니다." })
     }
     // 예외 케이스에서 처리하지 못한 에러
     catch (error) {
